a5a9405ece13ac1193cbb75669f5f366bce5cfab
Roy Tewalt <Roy.Tewalt@target.com>
2018-11-09 16:55:39 -0600

    add name and email to git (#2)

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,5 +5,9 @@ set -Eeuo pipefail
 PLUGIN_MODULE="${PLUGIN_MODULE:-}"
 PLUGIN_RUN_DIR="${PLUGIN_RUN_DIR:-}"
 
+git config --global user.name "drone"
+git config --global user.email "drone@drone.shipt.com"
+
 git fetch --no-tags origin $DRONE_BRANCH
+
 git --no-pager diff --unified=0 origin/$DRONE_BRANCH $PLUGIN_MODULE | $PLUGIN_RUN_DIR/plugin
